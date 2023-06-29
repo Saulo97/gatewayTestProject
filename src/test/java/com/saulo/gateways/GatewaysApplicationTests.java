7fc6648b9666b9b807f35cb25e20ef0d24231e7d
Saulo0097 <saulhg.2015@gmail.com>
2023-06-29 10:29:30 -0400

    config project

---
+++ src/test/java/com/saulo/gateways/GatewaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saulo.gateways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
