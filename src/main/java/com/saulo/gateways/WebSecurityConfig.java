b8e2340363b2a2d230670e6a7622177715414958
Saulo0097 <saulohermida1997@gmail.com>
2023-12-29 23:08:55 -0500

    add future security

---
+++ src/main/java/com/saulo/gateways/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.saulo.gateways;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // require all requests to be authenticated except for the resources
+        http.authorizeHttpRequests((requests)->requests.anyRequest().permitAll());
+        // not needed as JSF 2.2 is implicitly protected against CSRF
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}
